9152d2e78180a201e4e061d9b41d7b8a06c3484c
Arfin Foysal <arfinfoysalbd@gmail.com>
2022-02-13 12:06:50 +0600

    check

--- algorithms.js
+++ algorithms.js
@@ -1,3 +1,4 @@
+
 const arr = [1, 5, 21, 5, 3, 56, 56, 52, 356, 80, 855];
 
 // babblesorte
@@ -16,7 +17,6 @@ const arr = [1, 5, 21, 5, 3, 56, 56, 52, 356, 80, 855];
 // console.log("[sorted]", arr);
 // console.log(arr);
 
-
 // insertionSort
 
 // function insertionSort(inputArr) {
@@ -25,7 +25,7 @@ const arr = [1, 5, 21, 5, 3, 56, 56, 52, 356, 80, 855];
 //             // Choosing the first element in our unsorted subarray
 //             let current = inputArr[i];
 //             // The last element of our sorted subarray
-//             let j = i-1; 
+//             let j = i-1;
 //             while ((j > -1) && (current < inputArr[j])) {
 //                 inputArr[j+1] = inputArr[j];
 //                 j--;
@@ -41,28 +41,28 @@ const arr = [1, 5, 21, 5, 3, 56, 56, 52, 356, 80, 855];
 
 // selectionSort
 
-function selectionSort(inputArr) { 
-    let n = inputArr.length;
-        
-    for(let i = 0; i < n; i++) {
-        // Finding the smallest number in the subarray
-        let min = i;
-        for(let j = i+1; j < n; j++){
-            if(inputArr[j] < inputArr[min]) {
-                min=j; 
-            }
-         }
-         if (min != i) {
-             // Swapping the elements
-             let tmp = inputArr[i]; 
-             inputArr[i] = inputArr[min];
-             inputArr[min] = tmp;      
-        }
+function selectionSort(inputArr) {
+  let n = inputArr.length;
+
+  for (let i = 0; i < n; i++) {
+    // Finding the smallest number in the subarray
+    let min = i;
+    for (let j = i + 1; j < n; j++) {
+      if (inputArr[j] < inputArr[min]) {
+        min = j;
+      }
     }
-    return inputArr;
+    if (min != i) {
+      // Swapping the elements
+      let tmp = inputArr[i];
+      inputArr[i] = inputArr[min];
+      inputArr[min] = tmp;
+    }
+  }
+  return inputArr;
 }
 
-selectionSort(arr)
+selectionSort(arr);
 
 console.log(arr);
 
@@ -87,11 +87,14 @@ console.log(arr);
 // }
 
 // linearSearch
-function linearSearch(arr, key){
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] === key){
-            return i
-        }
+function linearSearch(arr, key) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === key) {
+      return i;
     }
-    return -1
+  }
+  return -1;
 }
+
+
+// $$$$$$$$$$$$$$$$$$$ arfin foysal$$$$$$$$$$$$$$$$$$
